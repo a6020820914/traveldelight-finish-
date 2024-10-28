3c9c0e52183599c84ab87e8db33032cfd2a6a82b
盧奕誠 <luyicheng@luyichengdebijixingdiannao.local>
2024-10-28 17:26:13 +0800

    make it better

--- members/views.py
+++ members/views.py
@@ -1062,6 +1062,8 @@ def fetch_train_schedule(start_station, end_station, ride_date):
     # 初始化 Chrome 瀏覽器
     options = webdriver.ChromeOptions()
     options.add_argument("--headless")
+    options.add_argument("--no-sandbox")
+    options.add_argument("--disable-dev-shm-usage")
 
     driver = webdriver.Chrome(options=options)
 
